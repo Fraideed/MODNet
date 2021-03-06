07fdff6bedd1503b0c2c11785a658f54546f68b1
Fraideed <2690632679@qq.com>
2021-03-06 13:22:28 +0800

    refine

--- inference.py
+++ inference.py
@@ -1,5 +1,4 @@
 import os
-import argparse
 import numpy as np
 from PIL import Image
 import cv2
@@ -10,8 +9,6 @@ import torch.nn.functional as F
 import torchvision.transforms as transforms
 
 from src.models.modnet import MODNet
-
-
 
 input_path='inputs'
 output_path='outputs'
@@ -44,6 +41,25 @@ modnet = nn.DataParallel(modnet).cuda()
 modnet.load_state_dict(torch.load(ckpt_path))
 modnet.eval()
 
+
+def save_fg(img_data,matte_data,filename):
+    w,h = img_data.width,img_data.height
+    img_data = np.asarray(img_data)
+    if len(img_data.shape) ==2:
+       img_data = img_data[:,:,None]
+    if img_data.shape[2]==1:
+       img_data = np.repeat(img_data,3,axis=2)
+    elif img_data.shape[2] ==4:
+       img_data = img_data[:,:,0:3]
+    matte = np.repeat(matte_data[:,:,None],3,axis=2)
+    # fg_black = matte*img_data#+(1-matte)*np.full(img_data.shape,0)
+    fg_white = matte*img_data+(1-matte)*np.full(img_data.shape,255)
+    # combined = np.concatenate((img_data,matte*255,fg_black,fg_white),axis=1)
+    output = Image.fromarray(np.uint8(fg_white))
+    output.save(filename)
+    return
+
+
 # inference images
 im_names = os.listdir(input_path)
 for im_name in im_names:
@@ -52,7 +68,6 @@ for im_name in im_names:
     # read image
     im = Image.open(os.path.join(input_path, im_name))
     ori=im.copy()
-    (h,w)=ori.size
     # unify image channels to 3
     im = np.asarray(im)
     if len(im.shape) == 2:
@@ -92,14 +107,33 @@ for im_name in im_names:
     # resize and save matte
     matte = F.interpolate(matte, size=(im_h, im_w), mode='area')
     matte = matte[0][0].data.cpu().numpy()
-    matte_name = im_name.split('.')[0] + '_output.png'
-    mask=Image.fromarray(((matte * 255).astype('uint8')), mode='L')    #.save(os.path.join(args.output_path, matte_name))
-    ori = cv2.cvtColor(np.asarray(ori), cv2.COLOR_RGB2BGR)
-    mask = cv2.cvtColor(np.asarray(mask), cv2.COLOR_RGB2BGR)
-    mask=cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
-    ori=cv2.resize(ori,(mask.shape[1],mask.shape[0]))
-    ori[mask == 0] = (255, 255, 255)
-    output=cv2.resize(ori,(w,h))
-    cv2.imwrite(os.path.join(output_path, matte_name),ori)
+    print(matte)
+    matte_name = im_name.split('.')[0] + '_mask.png'
+    res_name=im_name.split('.')[0] + '_result.png'
+    # print(matte.shape)
+    # fg = matte * ori + (1 - matte) * np.full(ori.shape, 0.0)
+    Image.fromarray(((matte*255).astype('uint8')), mode='L').save(os.path.join(output_path, matte_name))
+    save_fg(ori,matte,os.path.join(output_path,res_name))
+    # print(matte[0])
+    # matte= matte.convert("RGB")
+    # print(matte.size)
+    # fg = matte * ori + (1-matte)*np.full(ori.shape, 0.0)
+    # print(matte.size)
+    # matte= cv2.cvtColor(np.asarray(matte), cv2.COLOR_RGB2BGR)
+    # img = cv2.imread(os.path.join(input_path, im_name))
+    # matting = cv2.imread(matte, cv2.IMREAD_GRAYSCALE)
+    # fg = matte * img + (1 - matte) * np.full(img.shape, 0.0)
+    # ori = Image.fromarray(((matte * 255).astype('uint8')))
+    # ori=Image.fromarray(ori)
+    # fg = matte * ori + (1 - matte) * np.full(ori.shape, 0.0)
+    # fg.save(os.path.join(output_path, matte_name))
+    # mask.save('mask.png')
+    # ori = cv2.cvtColor(np.asarray(ori), cv2.COLOR_RGB2BGR)
+    # mask = cv2.cvtColor(np.asarray(mask), cv2.COLOR_RGB2BGR)
+    # mask=cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
+
+    # cv2.imwrite('mask2.png',mask)
+    # ori[mask == 0] = (255, 255, 255)
+    # cv2.imwrite(os.path.join(output_path, matte_name),ori)
 
 
